cae27648030d2600ab9cb86f925082f509ab364b
PrimeBuild-pc <primebuild.official@gmail.com>
2026-04-16 01:54:18 +0200

    chore(release): prepare 1.1.2 manifests for winget and chocolatey

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -2,12 +2,15 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'threadpilot'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url64 = 'https://github.com/PrimeBuild-pc/ThreadPilot/releases/download/v1.1.1/ThreadPilot_v1.1.1_Setup.exe'
+$url64 = 'https://github.com/PrimeBuild-pc/ThreadPilot/releases/download/v1.1.2/ThreadPilot_v1.1.2_Setup.exe'
+$checksum64 = 'fac3935ba126b01987b15cb323502ee8b6e91c957f22006bd9e62b8d57169790'
 
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'exe'
     url64bit       = $url64
+    checksum64     = $checksum64
+    checksumType64 = 'sha256'
     softwareName   = 'ThreadPilot*'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
     validExitCodes = @(0)
