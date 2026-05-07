30a572a9da4c1b0d4baae1675115cfb11352f21c
PrimeBuild-pc <primebuild.official@gmail.com>
2026-05-07 02:10:04 +0200

    Update Chocolatey package to v1.1.2.1
    Bump threadpilot Chocolatey package to 1.1.2.1 and include fix artifact. Adds threadpilot-1.1.2.1-fix.zip and placeholder chocolatey files (cd, choco). Simplifies silent install arguments and adds exit code 1 to validExitCodes in chocolateyInstall.ps1 to better handle installer return values. Also add CHOCOLATEY_MODERATOR_NOTICE.md to .gitignore.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -13,8 +13,8 @@ $packageArgs = @{
     checksum64     = $checksum64
     checksumType64 = 'sha256'
     softwareName   = 'ThreadPilot*'
-    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS=0'
-    validExitCodes = @(0, 3010, 1641)
+    silentArgs     = '/VERYSILENT /SP- /NORESTART'
+    validExitCodes = @(0, 1, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
