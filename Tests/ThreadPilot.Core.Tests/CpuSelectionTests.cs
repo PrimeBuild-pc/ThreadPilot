3752abb06ab1f4224b947fb0fbdb223538eff7ab
PrimeBuild-pc <primebuild.official@gmail.com>
2026-05-19 16:21:14 +0200

    Expand CpuSelection test coverage

--- Tests/ThreadPilot.Core.Tests/CpuSelectionTests.cs
+++ Tests/ThreadPilot.Core.Tests/CpuSelectionTests.cs
@@ -73,6 +73,13 @@ namespace ThreadPilot.Core.Tests
         }
 
         [Fact]
+        public void CpuTopologySnapshot_Create_ThrowsWhenLogicalProcessorsIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                CpuTopologySnapshot.Create(null!));
+        }
+
+        [Fact]
         public void CpuTopologySnapshot_PerformanceEfficiencyClass_IsHighestNumericValue()
         {
             var eCore = new ProcessorRef(0, 8, 8);
@@ -86,6 +93,16 @@ namespace ThreadPilot.Core.Tests
                 });
 
             Assert.Equal<byte?>(2, topology.GetPerformanceEfficiencyClass());
+        }
+
+        [Fact]
+        public void CpuTopologySnapshot_GetPerformanceEfficiencyClass_ReturnsNullWhenNoEfficiencyClassesExist()
+        {
+            var topology = CpuTopologySnapshot.Create([new ProcessorRef(0, 0, 0)]);
+
+            var performanceClass = topology.GetPerformanceEfficiencyClass();
+
+            Assert.Null(performanceClass);
         }
 
         [Fact]
@@ -105,6 +122,27 @@ namespace ThreadPilot.Core.Tests
         }
 
         [Fact]
+        public void FromLegacyAffinityMask_WithCpuSetId_SetsMigrationMetadataAndIndexes()
+        {
+            var cpu0 = new ProcessorRef(0, 0, 0);
+            var cpu2 = new ProcessorRef(0, 2, 2);
+            var topology = CpuTopologySnapshot.Create(
+                [cpu0, cpu2],
+                new Dictionary<ProcessorRef, uint>
+                {
+                    [cpu0] = 300,
+                    [cpu2] = 100,
+                });
+
+            var selection = CpuSelection.FromLegacyAffinityMask(0b101, topology);
+
+            Assert.True(selection.Metadata.CreatedFromLegacyAffinityMask);
+            Assert.Equal("Migrated from legacy affinity mask", selection.Metadata.SelectionReason);
+            Assert.Equal([0, 2], selection.GlobalLogicalProcessorIndexes);
+            Assert.Equal([100U, 300U], selection.CpuSetIds);
+        }
+
+        [Fact]
         public void ToLegacyAffinityMaskOrNull_ReturnsMaskForSingleGroupBelow64()
         {
             var topology = CpuTopologySnapshot.Create(
@@ -120,6 +158,22 @@ namespace ThreadPilot.Core.Tests
         }
 
         [Fact]
+        public void FromProcessors_ThrowsWhenProcessorsIsNull()
+        {
+            var topology = CpuTopologySnapshot.Create([new ProcessorRef(0, 0, 0)]);
+
+            Assert.Throws<ArgumentNullException>(() =>
+                CpuSelection.FromProcessors(null!, topology));
+        }
+
+        [Fact]
+        public void FromProcessors_ThrowsWhenTopologyIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                CpuSelection.FromProcessors([new ProcessorRef(0, 0, 0)], null!));
+        }
+
+        [Fact]
         public void FromProcessors_ThrowsWhenProcessorIsNotInTopology()
         {
             var topology = CpuTopologySnapshot.Create([new ProcessorRef(0, 0, 0)]);
@@ -129,6 +183,33 @@ namespace ThreadPilot.Core.Tests
                 CpuSelection.FromProcessors([missingProcessor], topology));
 
             Assert.Contains("topology", exception.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void FromProcessors_WithCpuSetIds_PopulatesDistinctOrderedCpuSetIds()
+        {
+            var cpu0 = new ProcessorRef(0, 0, 0);
+            var cpu1 = new ProcessorRef(0, 1, 1);
+            var cpu2 = new ProcessorRef(0, 2, 2);
+            var topology = CpuTopologySnapshot.Create(
+                [cpu0, cpu1, cpu2],
+                new Dictionary<ProcessorRef, uint>
+                {
+                    [cpu0] = 200,
+                    [cpu1] = 100,
+                    [cpu2] = 200,
+                });
+
+            var selection = CpuSelection.FromProcessors([cpu0, cpu1, cpu2], topology);
+
+            Assert.Equal([100U, 200U], selection.CpuSetIds);
+        }
+
+        [Fact]
+        public void ToLegacyAffinityMaskOrNull_ThrowsWhenSelectionIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                CpuSelection.ToLegacyAffinityMaskOrNull(null!));
         }
 
         [Fact]
